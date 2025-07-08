3b471c0d7be9ce49aab207992591d03f11512a8d
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-08 20:54:22 -0300

    feat: funcionario create dto is work

--- src/funcionarios/dto/create-funcionario.dto.ts
+++ src/funcionarios/dto/create-funcionario.dto.ts
@@ -4,4 +4,22 @@ export class CreateFuncionarioDto{
     @IsString()
     @IsNotEmpty()
     nome: String
-}
+   
+    @IsNotEmpty()  
+    @IsEmail()  
+    email: String
+    @IsString()
+  @IsNotEmpty()
+  @MinLength(11, { message: 'CPF deve ter no mínimo 11 caracteres (apenas números).' }) 
+  cpf: string;
+
+  @IsString() 
+  @IsNotEmpty()
+  admissao: string; 
+
+  @IsInt()
+  @IsNotEmpty()
+  carga_horaria: number;
+}     
+
+
