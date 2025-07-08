5d1babfa32de758b5f019e630b7aa9d69ed17e29
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-07 21:25:28 -0300

    feat: create userdto

---
+++ src/user/user.dto.ts
@@ -0,0 +1,10 @@
+export class UserDto{
+    id:string;
+    nome:string;
+    email:string;
+    cpf: number;
+    ativo: boolean;
+    carga_horaria: number;
+    data_contratacao: Date;
+    senha: string;
+}
