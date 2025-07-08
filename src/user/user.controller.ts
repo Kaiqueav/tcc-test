55a27d59c74c04c2446a53029fd6a6316fd2dcb2
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-07 21:21:24 -0300

    feat: user setup create

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+constructor( private userService:UserService){}
+    @Get()
+    findAll(): string{ return this.userService.findAll()};
+    
+}
