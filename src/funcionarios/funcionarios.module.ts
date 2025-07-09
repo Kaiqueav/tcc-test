27803c529523616cf16a096eee1ec7bfff3110a4
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-08 21:52:37 -0300

    feat: config module

--- src/funcionarios/funcionarios.module.ts
+++ src/funcionarios/funcionarios.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Funcionario } from './entities/funcionario.entity';
+import { FuncionariosService } from './funcionarios.service';
+import { FuncionariosController } from './funcionarios.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Funcionario])], // Register Funcionario entity with TypeORM
+  controllers: [FuncionariosController],
+  providers: [FuncionariosService],
+  exports: [FuncionariosService, TypeOrmModule],})
 export class FuncionariosModule {}
+
