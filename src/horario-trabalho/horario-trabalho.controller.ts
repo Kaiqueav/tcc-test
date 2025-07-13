037bb7c5612294ca1cf12b2242d07278fc28dbc9
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-13 16:05:05 -0300

    feat: controller horario-travalho

--- src/horario-trabalho/horario-trabalho.controller.ts
+++ src/horario-trabalho/horario-trabalho.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { HorarioTrabalhoService } from './horario-trabalho.service';
+import { CreateHorarioTrabalhoDTO } from './dto/create-horario-trabalho.dto';
+import { UpdateHorarioTrabalhoDTO } from './dto/update-horario-trabalho.dto';
 
 @Controller('horario-trabalho')
-export class HorarioTrabalhoController {}
+export class HorarioTrabalhoController {
+     constructor(private readonly horarioTrabalhoService: HorarioTrabalhoService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createHorarioTrabalhoDto: CreateHorarioTrabalhoDTO) {
+    return this.horarioTrabalhoService.create(createHorarioTrabalhoDto);
+  }
+  
+  @Get('funcionario/:id')
+  findAllByFuncionario(@Param('id', ParseIntPipe) id: number) {
+    return this.horarioTrabalhoService.findAllByFuncionario(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateHorarioTrabalhoDto: UpdateHorarioTrabalhoDTO) {
+    return this.horarioTrabalhoService.update(id, updateHorarioTrabalhoDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.horarioTrabalhoService.remove(id);
+  }
+}
