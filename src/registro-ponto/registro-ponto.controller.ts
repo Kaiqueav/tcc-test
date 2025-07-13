0a1d94c44009c081b8b70d1617fa3cb3ba186622
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-13 12:48:51 -0300

    feat: controller

--- src/registro-ponto/registro-ponto.controller.ts
+++ src/registro-ponto/registro-ponto.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { RegistroPontoService } from './registro-ponto.service';
+import { CreateRegistroPontoDto } from './dto/create-registro-ponto.dto';
+import { RegistroPonto } from './entities/registro-ponto-entity';
+import { UpdateFuncionarioDto } from 'src/funcionarios/dto/update-funcionario.dto';
+import { UpdateRegistroPontoDto } from './dto/update-registro-ponto';
 
 @Controller('registro-ponto')
-export class RegistroPontoController {}
+export class RegistroPontoController {
+    constructor( private readonly registroPontoService : RegistroPontoService ){}
+// rota de criação: POST/ registro-ponto
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() createRegistroPontoDto : CreateRegistroPontoDto): Promise<RegistroPonto>
+    {
+        return this.registroPontoService.create(createRegistroPontoDto);
+    }
+// Listar todos os registros ROTA: /registro-ponto
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<RegistroPonto[]>
+    {
+        return this.registroPontoService.findAll();
+    }
+    // Listar todos os registro de um funcionario ROTA: /registro-ponto/funcionario/5
+    @Get('funcionario/:funcionarioId')
+    @HttpCode(HttpStatus.OK) 
+    findAllByFuncionario(@Param('funcionarioId', ParseIntPipe) funcionarioId:number): Promise<RegistroPonto[]>
+    {
+        return this.registroPontoService.findAllByFuncionario(funcionarioId)
+    }
+
+    // Buscar registro especifico por um id. ROTA: /registro-ponto/1
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    findOne(@Param('id', ParseIntPipe) id:number): Promise<RegistroPonto>
+    {
+        return this.registroPontoService.findOne(id);
+    }
+
+    // atualizar funcionario
+    @Patch(':id')
+    @HttpCode(HttpStatus.OK)
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateRegistroPontoDto: UpdateRegistroPontoDto):Promise<RegistroPonto>
+    {
+        return this.registroPontoService.update(id, updateRegistroPontoDto    );
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    remove(@Param('id', ParseIntPipe) id: number): Promise<void>
+    {
+        return this.registroPontoService.remove(id);
+    }
+    
+}
