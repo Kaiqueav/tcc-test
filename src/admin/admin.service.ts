3f5e1971b1843894285c694d6f361a222f533ab3
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-18 21:57:50 -0300

    feat: create service for admin on server

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import * as bcrypt from 'bcrypt';
+import { Adm } from './entities/admin.entity';
 
 @Injectable()
-export class AdminService {}
+export class AdmService {
+    constructor( 
+        @InjectRepository(Adm)
+        private readonly admRepository:Repository<Adm>,
+    ){}
+
+    async createAdmin(createAdminDto: CreateAdminDto): Promise<Adm>{
+            const saltRound = 10;
+            const hashedPassword = await bcrypt.hash(createAdminDto.password, saltRound);
+
+            const newAdmin = new Adm();
+            newAdmin.email = createAdminDto.email;
+            newAdmin.password = hashedPassword
+
+            return this.admRepository.save(newAdmin)
+    }   
+     async findOneByEmail(email: string): Promise<Adm| undefined> {
+        return this.admRepository.findOne({ where: { email } });
+    }
+}
