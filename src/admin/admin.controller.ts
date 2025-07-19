9bb670331370f3da0879e9f89a1f81722486c752
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-07-18 21:57:58 -0300

    create controller

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { AdmService } from './admin.service';
+import { Admin } from 'typeorm';
+import { Adm } from './entities/admin.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { AdminGuard } from 'src/auth/admin.guard';
 
+@UseGuards(AuthGuard('jwt'), AdminGuard) // Protege todas as rotas
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+
+  constructor(private readonly adminService: AdmService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createAdminDto: CreateAdminDto): Promise<{ message: string; admin: Admin }> {
+    const admin = await this.adminService.createAdmin(createAdminDto);
+    delete admin.password; // Remove a senha da resposta por segurança
+    return { message: 'Administrador criado com sucesso.', admin: admin as any};
+  }
+}
