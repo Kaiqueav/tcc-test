ed8401b4c4b372f85d38899a4d005c22aa462ad3
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-08-12 19:50:23 -0300

    configuração do cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AllExceptionsFilter } from './common/filters/http-exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors();
+ app.enableCors({
+    origin: true, // Permite qualquer origem (ótimo para desenvolvimento)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   app.useGlobalFilters(new AllExceptionsFilter());
